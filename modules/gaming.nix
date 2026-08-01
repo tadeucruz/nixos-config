1a1fa6012b06ba6af2085731014b4e4ce6dd3652
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-01 01:45:26 -0300

    modules/gaming.nix: drop proton-ge-bin, managing it via protonplus instead

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,17 +1,20 @@
 # Shared gaming stack (citadel + g15 + legion). Gamescope/session integration
 # is handled separately by Jovian on legion; this covers the generic Steam
 # features useful everywhere.
-{ pkgs, ... }:
+{ ... }:
 {
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  hardware = {
+    steam-hardware.enable = true;
+    xpadneo.enable = true;
   };
 
-  programs.gamemode.enable = true;
+  programs = {
+    gamemode.enable = true;
 
-  hardware.steam-hardware.enable = true;
-  hardware.xpadneo.enable = true;
+    steam = {
+      enable = true;
+      localNetworkGameTransfers.openFirewall = true;
+      remotePlay.openFirewall = true;
+    };
+  };
 }
