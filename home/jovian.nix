9a3b1400f7f7d260f48d693301c7865584015ec0
Tadeu Cruz <tadeucruz@gmail.com>
2026-07-31 23:33:59 -0300

    legion: revert InputPlumber generic-gamepad override, drop return-to-steam entry

--- home/jovian.nix
+++ home/jovian.nix
@@ -4,13 +4,4 @@
   home.packages = with pkgs; [
     protonplus
   ];
-
-  xdg.desktopEntries.return-to-steam = {
-    name = "Return to Steam";
-    comment = "Switch back to Steam Big Picture session";
-    exec = "start-gamescope-session";
-    icon = "steam";
-    terminal = false;
-    categories = [ "Game" ];
-  };
 }
