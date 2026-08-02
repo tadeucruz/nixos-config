93807fd3812c7a20e55cba5c1127b7e842ff5003
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-01 22:06:29 -0300

    legion: restore return-to-steam desktop icon
    Dropped unintentionally in 9a3b140 alongside an unrelated InputPlumber
    revert; the entry moved out of home/jovian.nix in the later reorg
    never brought it back.

--- home/legion.nix
+++ home/legion.nix
@@ -4,4 +4,13 @@
     ./common.nix
     ./gaming.nix
   ];
+
+  xdg.desktopEntries.return-to-steam = {
+    name = "Return to Steam";
+    comment = "Switch back to Steam Big Picture session";
+    exec = "start-gamescope-session";
+    icon = "steam";
+    terminal = false;
+    categories = [ "Game" ];
+  };
 }
