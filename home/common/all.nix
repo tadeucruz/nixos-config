d29f5e73c4adcaf6a3ab5154c960dc7ac6199e6d
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-16 12:26:38 -0300

    starship: add shared complete prompt config

--- home/common/all.nix
+++ home/common/all.nix
@@ -54,7 +54,60 @@
 
     home-manager.enable = true;
 
-    starship.enable = true;
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+
+      settings = {
+        add_newline = true;
+        format = "╭─$os$username$hostname$directory$git_branch$git_status$go$java$dart$cmd_duration$jobs$line_break╰─$character";
+
+        os = { disabled = false; style = "bold blue"; };
+
+        username = {
+          show_always = true;
+          style_user = "bold yellow";
+          style_root = "bold red";
+          format = "[$user]($style) ";
+        };
+
+        hostname = {
+          disabled = false;
+          ssh_only = false;
+          style = "bold green";
+          format = "[@$hostname](bold green) ";
+        };
+
+        directory = {
+          truncation_length = 4;
+          truncation_symbol = "…/";
+          read_only = "🔒";
+          style = "bold cyan";
+        };
+
+        git_branch = {
+          symbol = " ";
+          style = "bold purple";
+        };
+        git_status = { style = "bold red"; };
+
+        go = { symbol = " "; };
+        java = { symbol = " "; };
+        dart = { symbol = " "; };
+
+        cmd_duration = {
+          min_time = 2000;
+          format = "took [$duration](bold yellow) ";
+        };
+        jobs = { symbol = "✦ "; };
+
+        character = {
+          success_symbol = "[❯](bold green)";
+          error_symbol = "[❯](bold red)";
+          vimcmd_symbol = "[❮](bold green)";
+        };
+      };
+    };
 
     zsh = {
       enable = true;
