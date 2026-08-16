e74c99b54720e60ff206667cdc7cfc470898cad0
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-16 13:14:29 -0300

    darwin: switch nh to home-manager programs.nh with GC enabled
    nh was just a plain home.packages entry, so unlike the NixOS hosts
    (programs.nh.clean.enable via modules/common/linux.nix) the nix
    store on normandy was never garbage collected automatically. Use
    home-manager's programs.nh module instead, matching the same
    clean.extraArgs used on Linux.

--- home/common/darwin.nix
+++ home/common/darwin.nix
@@ -7,7 +7,6 @@
       SSH_AUTH_SOCK = "/Users/${username}/.bitwarden-ssh-agent.sock";
     };
     packages = with pkgs; [
-      nh
       bitwarden-desktop
       betterdisplay
       obsidian
@@ -15,5 +14,15 @@
     ];
   };
 
-  programs.zsh.shellAliases.rebuild = "cd $NH_DARWIN_FLAKE && git pull && nh darwin switch -H ${hostname}";
+  programs = {
+    nh = {
+      enable = true;
+      clean = {
+        enable = true;
+        extraArgs = "--keep-since 4d --keep 3";
+      };
+      darwinFlake = "/Users/${username}/nixos-config";
+    };
+    zsh.shellAliases.rebuild = "cd $NH_DARWIN_FLAKE && git pull && nh darwin switch -H ${hostname}";
+  };
 }
