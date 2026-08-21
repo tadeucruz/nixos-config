6516d582846ef76d128bab4ad3bfcd8cc419eb8a
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-21 00:21:25 -0300

    citadel: manage return-to-steam desktop shortcut via home-manager

--- home/hosts/citadel.nix
+++ home/hosts/citadel.nix
@@ -1,4 +1,19 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  # Written to both the applications dir (via xdg.desktopEntries) and the
+  # Desktop (via home.file). The Desktop symlink used to be manual and pointed
+  # into an old home-manager generation that GC removed -> broken shortcut.
+  return-to-steam-desktop = pkgs.writeText "return-to-steam.desktop" ''
+    [Desktop Entry]
+    Type=Application
+    Name=Return to Steam
+    Comment=Switch back to Steam Big Picture session
+    Exec=start-gamescope-session
+    Icon=steam
+    Terminal=false
+    Categories=Game;
+  '';
+in
 {
   imports = [
     ../common/all.nix
@@ -11,12 +26,13 @@
     SteamDeck = "0";
   };
 
-  xdg.desktopEntries.return-to-steam = {
-    name = "Return to Steam";
-    comment = "Switch back to Steam Big Picture session";
-    exec = "start-gamescope-session";
-    icon = "steam";
-    terminal = false;
-    categories = [ "Game" ];
+  home.file = {
+    # Managed by home-manager in both spots so the Desktop symlink survives GC
+    # (a manual ~/Desktop symlink used to point into an old generation).
+    ".local/share/applications/return-to-steam.desktop".source = return-to-steam-desktop;
+    "Desktop/return-to-steam.desktop" = {
+      source = return-to-steam-desktop;
+      force = true;
+    };
   };
 }
