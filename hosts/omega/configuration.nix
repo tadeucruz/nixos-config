75f570d73765b93877d4b603b72dd315c07b3fae
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-29 17:02:40 -0300

    omega: static IP 192.168.0.2 (scripted networking, drop NetworkManager)

--- hosts/omega/configuration.nix
+++ hosts/omega/configuration.nix
@@ -18,7 +18,26 @@
     systemd-boot.enable = true;
   };
 
-  networking.hostName = hostname;
+  # Static IP: wired-only headless server. NetworkManager is overkill for a
+  # single NIC, so use NixOS scripted networking instead.
+  networking = {
+    hostName = hostname;
+    networkmanager.enable = lib.mkForce false;
+    useDHCP = false;
+
+    interfaces.enp1s0 = {
+      useDHCP = false;
+      ipv4.addresses = [
+        {
+          address = "192.168.0.2";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    defaultGateway = "192.168.0.1";
+    nameservers = [ "1.1.1.1" ];
+  };
 
   system.stateVersion = "26.05";
 }
