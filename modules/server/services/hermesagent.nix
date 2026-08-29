6bec5b9a72fe497099dcb071274f98f499d8306e
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-29 13:32:46 -0300

    hermesagent: use Debian trixie (stable) instead of bookworm

--- modules/server/services/hermesagent.nix
+++ modules/server/services/hermesagent.nix
@@ -1,10 +1,8 @@
 # HermesAgent LXC container (omega). LXC rootfs is imperative (unlike NixOS
-# containers): created once on first boot via the download template, then
-# autostarted by lxc.service on every boot. The container config is
-# declarative (written from this repo). Requires the generic LXC module
-# (modules/server/lxc.nix).
-#
-# TODO: swap the distro below if HermesAgent needs something else (Alpine etc.).
+# containers): created once on first boot via the Debian trixie download
+# template, then autostarted by lxc.service on every boot. The container
+# config is declarative (written from this repo). Requires the generic LXC
+# module (modules/server/lxc.nix).
 {
   config,
   pkgs,
@@ -46,7 +44,7 @@ in
     };
     script = ''
       if [ ! -d /var/lib/lxc/hermesagent ]; then
-        ${pkgs.lxc}/bin/lxc-create -n hermesagent -t download -- -d debian -r bookworm -a amd64
+        ${pkgs.lxc}/bin/lxc-create -n hermesagent -t download -- -d debian -r trixie -a amd64
         cp ${hermesagentConfig} /var/lib/lxc/hermesagent/config
       fi
       ${pkgs.lxc}/bin/lxc-start -n hermesagent || true
