0db7496d7dc8407787d86dc73299ffc5e257091e
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-29 23:20:34 -0300

    ollama: set VK_ICD_FILENAMES so vulkan backend finds RADV on the iGPU

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -5,6 +5,9 @@
     package = pkgs.ollama-vulkan;
     host = "0.0.0.0";
     openFirewall = true;
+    environmentVariables = {
+      VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+    };
     loadModels = [ "deepseek-r1:7b" ];
     syncModels = true;
   };
