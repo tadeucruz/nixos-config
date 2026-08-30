45f4388a1c57a8dedd4c4f984e8e52bef13e7ce0
Tadeu Cruz <tadeucruz@gmail.com>
2026-08-29 22:33:30 -0300

    haos: use existing domain uuid so virsh define updates (idempotent)

--- modules/services/haos.nix
+++ modules/services/haos.nix
@@ -21,7 +21,7 @@ in
   environment.etc."libvirt/qemu/haos.xml".text = ''
     <domain type='kvm'>
       <name>haos</name>
-      <uuid>193a192e-bc8a-4951-b3fd-d72627fefd24</uuid>
+      <uuid>fd42671f-5507-4ae5-ae91-f333557a2d4e</uuid>
       <memory unit='GiB'>4</memory>
       <vcpu>4</vcpu>
       <os>
