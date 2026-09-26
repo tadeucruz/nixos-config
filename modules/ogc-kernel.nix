a1ee848a29ee0604b0d1f54d9f3c5e34d0ca9a1f
tadeucruz <3186048+tadeucruz@users.noreply.github.com>
2026-09-26 14:39:07 +0000

    chore: bump dependencies

--- modules/ogc-kernel.nix
+++ modules/ogc-kernel.nix
@@ -1,11 +1,11 @@
 { pkgs, lib, ... }:
 
 let
-  ogcRelease = "v7.2.1-ogc3";
+  ogcRelease = "v7.2.7-ogc1";
 
   ogcPatch = pkgs.fetchurl {
     url = "https://github.com/OpenGamingCollective/linux/releases/download/${ogcRelease}/monolithic.patch";
-    hash = "sha256-RCGhHnCJIx+Ori+TPwRBarAcFMBxDY3IS1zvDXBmDSM=";
+    hash = "sha256-SR06wNMTNFOTeUyqd/Q0abCX+FqOF8QonTzhsl2PQCE=";
   };
 
   ogcKernel = pkgs.linux_latest.override {
